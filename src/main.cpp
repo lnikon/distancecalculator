99be0e0bcddd78237801d4de9999563927c70aac
lnikon <bejanyan.vahag@protonmail.com>
2020-09-14 22:36:44 +0400

    First Catch2 unit test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "hello.h"
 
 #include <iostream>
+#include <optional>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
     std::cout << ns_hello::hello("Initial setup");
+    
     return 0;
 }
--- test/testmain.cpp
+++ test/testmain.cpp
@@ -1,23 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "hello.h"
 
-#include <iostream>
-
 #include <catch2/catch.hpp>
 
-namespace ns_testmain
-{
-
-bool testmain()
-{
-    const std::string msg = "my_message";
-    return ns_hello::hello(msg) == std::string{"Hello, " + msg + "\n"};    
+TEST_CASE( "Check ns_hello::hello(msg) return value", "[hello]" ) {
+    REQUIRE(ns_hello::hello("Name") == std::string{"Hello, Name\n"});
+    REQUIRE(ns_hello::hello("Name") != std::string{"Hello, Name"});
+    REQUIRE(ns_hello::hello("") == std::string{"Hello, \n"});
 }
-} // ns_testmain
-
-int main()
-{
-    std::cout << (ns_testmain::testmain() ? "PASS" : "FAIL") << "\n";
-    return 0;
-}
-
