3de3b3fa6a2249fe92f82b78db2b5272de5b5297
lnikon <bejanyan.vahag@protonmail.com>
2020-09-21 21:05:16 +0400

    Initial version of thread pool & job

--- src/structures/CSVContainer.hpp
+++ src/structures/CSVContainer.hpp
@@ -7,22 +7,32 @@ namespace structures
 {
 
 template <typename ValueType>
+using Row = std::vector<ValueType>;
+
+template <typename ValueType>
+using RowSPtr = std::shared_ptr<Row<ValueType>>;
+
+template <typename ValueType>
 class CSVContainer
 {
 public:
-    using Row = std::vector<ValueType>;
-
-    void append(const Row& row);
+    void append(RowSPtr<ValueType> row);
 
     std::size_t rowCount() const noexcept;
     std::size_t columnCount() const noexcept;
 
+    std::vector<RowSPtr<ValueType>>::iterator begin();
+    std::vector<RowSPtr<ValueType>>::iterator end();
+
+    std::vector<RowSPtr<ValueType>>::const_iterator cbegin();
+    std::vector<RowSPtr<ValueType>>::const_iterator cend();
+
 private:
-    std::vector<Row> m_data;
+    std::vector<RowSPtr<ValueType>> m_data;
 };
 
 template <typename ValueType>
-void CSVContainer<ValueType>::append(const std::vector<ValueType>& row)
+void CSVContainer<ValueType>::append(RowSPtr<ValueType> row)
 {
     m_data.push_back(row);
 }
@@ -38,10 +48,64 @@ std::size_t CSVContainer<ValueType>::columnCount() const noexcept
 {
     if (rowCount() >= 1)
     {
-        return m_data[0].size();
+        return m_data[0]->size();
     }
 
     return 0;
+}
+
+/*
+ * Members
+ */
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::iterator CSVContainer<ValueType>::begin()
+{
+    return m_data.begin();
+}
+
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::iterator CSVContainer<ValueType>::end()
+{
+    return m_data.end();
+}
+
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::const_iterator CSVContainer<ValueType>::cbegin()
+{
+    return m_data.cbegin();
+}
+
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::const_iterator CSVContainer<ValueType>::cend()
+{
+    return m_data.cend();
+}
+
+/*
+ * Free functions.
+ */
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::iterator begin(CSVContainer<ValueType>& c)
+{
+    return c.begin();
+}
+
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::iterator end(CSVContainer<ValueType>& c)
+{
+    return c.end();
+}
+
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::const_iterator cbegin(CSVContainer<ValueType>& c)
+{
+    return c.cbegin();
+}
+
+template <typename ValueType>
+std::vector<RowSPtr<ValueType>>::const_iterator cend(CSVContainer<ValueType>& c)
+{
+    return c.cend();
 }
 
 template <typename ValueType>
