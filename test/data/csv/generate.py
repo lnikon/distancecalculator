c1953d38e6b2a2e15457ddeaa8965311daa94222
lnikon <bejanyan.vahag@protonmail.com>
2020-10-03 13:53:26 +0400

    trying to fix generate.py super-lint issues

--- test/data/csv/generate.py
+++ test/data/csv/generate.py
@@ -4,12 +4,16 @@ from random import random
 import csv
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('rows', metavar='r', type=int, help='number of rows to generate') 
-    parser.add_argument('columns', metavar='c', type=int, help='number of columns to generate') 
+    parser.add_argument('rows', metavar='r', type=int,
+                        help='number of rows to generate')
+    parser.add_argument('columns', metavar='c', type=int,
+                        help='number of columns to generate')
     args = parser.parse_args()
     return args.rows, args.columns
+
 
 def write_csv(rows: int, columns: int):
     fieldnames = [str("p") + str(i) for i in range(columns)]
@@ -20,11 +24,11 @@ def write_csv(rows: int, columns: int):
             d[str("p") + str(i)] = random()
         writer.writerow(d)
 
+
 def main():
     rows, columns = parse_args()
     write_csv(rows, columns)
 
+
 if __name__ == '__main__':
     main()
-
-
