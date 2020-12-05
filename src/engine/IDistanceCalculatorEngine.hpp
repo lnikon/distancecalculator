ae5d878c5b1c80e026273611070166bf433f33cc
lnikon <bejanyan.vahag@protonmail.com>
2020-12-05 23:56:40 +0400

    minor code cleanup

--- src/engine/IDistanceCalculatorEngine.hpp
+++ src/engine/IDistanceCalculatorEngine.hpp
@@ -14,16 +14,4 @@ public:
                        calculate(structures::CSVContainerSPtr<ValueType> query,
                                  structures::CSVContainerSPtr<ValueType> dataset) const = 0;
     virtual EngineKind type() const = 0;
-
-    MetricKind metricKind() const
-    {
-        return m_metricKind;
-    }
-
-    void setMetricKind(const MetricKind metricKind)
-    {
-        m_metricKind = metricKind;
-    }
-
-    MetricKind m_metricKind{MetricKind::L1Metric};
 };
